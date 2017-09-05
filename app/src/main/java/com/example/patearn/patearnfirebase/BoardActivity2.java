71b3aa0c354f827b57b3b6a2c712928d870505d6
byungjoon <didils1982@gmail.com>
2017-09-06 00:11:38 +0900

    BoardActivity2 잘 동작 이제 변리사 소개 페이지를 조금 더 풍부하게 수정할 예정

--- app/src/main/java/com/example/patearn/patearnfirebase/BoardActivity2.java
+++ app/src/main/java/com/example/patearn/patearnfirebase/BoardActivity2.java
@@ -31,7 +31,7 @@ import java.util.List;
 public class BoardActivity2 extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private List<ImageDTO> imageDTOs = new ArrayList<>();
+    private List<AttorDTO> attorDTOs = new ArrayList<>();
     private List<String> uidLists = new ArrayList<>();
     private FirebaseDatabase database;
     private BoardRecyclerViewAdapter boardRecyclerViewAdapter;
@@ -51,15 +51,15 @@ public class BoardActivity2 extends AppCompatActivity {
         boardRecyclerViewAdapter = new BoardRecyclerViewAdapter();
         recyclerView.setAdapter(boardRecyclerViewAdapter);
 
-        database.getReference().child("images").addValueEventListener(new ValueEventListener() {
+        database.getReference().child("attorney").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                imageDTOs.clear();
+                attorDTOs.clear();
                 uidLists.clear();
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    ImageDTO imageDTO = snapshot.getValue(ImageDTO.class);
+                    AttorDTO attorDTO = snapshot.getValue(AttorDTO.class);
                     String uidkey = snapshot.getKey();                          //Firebase Database의 키 값
-                    imageDTOs.add(imageDTO);
+                    attorDTOs.add(attorDTO);
                     uidLists.add(uidkey);
                 }
                 boardRecyclerViewAdapter.notifyDataSetChanged();
@@ -83,16 +83,16 @@ public class BoardActivity2 extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-            ((CustomViewHolder)holder).textView.setText(imageDTOs.get(position).title);
-            ((CustomViewHolder)holder).textView2.setText(imageDTOs.get(position).description);
-            Glide.with(holder.itemView.getContext()).load(imageDTOs.get(position).imageUrl).into(((CustomViewHolder)holder).imageView);
+            ((CustomViewHolder)holder).textView.setText(attorDTOs.get(position).attorName);
+            ((CustomViewHolder)holder).textView2.setText(attorDTOs.get(position).description);
+            Glide.with(holder.itemView.getContext()).load(attorDTOs.get(position).imageUrl).into(((CustomViewHolder)holder).imageView);
             ((CustomViewHolder)holder).starButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    onStarClicked(database.getReference().child("images").child(uidLists.get(position)));
+                    onStarClicked(database.getReference().child("attorney").child(uidLists.get(position)));
                 }
             });
-            if (imageDTOs.get(position).stars.containsKey(mAuth.getCurrentUser().getUid())) {
+            if (attorDTOs.get(position).stars.containsKey(mAuth.getCurrentUser().getUid())) {
                 ((CustomViewHolder)holder).starButton.setImageResource(R.drawable.ic_favorite_black_48dp);
             } else {
                 ((CustomViewHolder)holder).starButton.setImageResource(R.drawable.ic_favorite_border_black_48dp);
@@ -108,30 +108,30 @@ public class BoardActivity2 extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return imageDTOs.size();
+            return attorDTOs.size();
         }
 
         private void onStarClicked(DatabaseReference postRef) {
             postRef.runTransaction(new Transaction.Handler() {
                 @Override
                 public Transaction.Result doTransaction(MutableData mutableData) {
-                    ImageDTO imageDTO = mutableData.getValue(ImageDTO.class);
-                    if (imageDTO == null) {
+                    AttorDTO attorDTO = mutableData.getValue(AttorDTO.class);
+                    if (attorDTO == null) {
                         return Transaction.success(mutableData);
                     }
 
-                    if (imageDTO.stars.containsKey(mAuth.getCurrentUser().getUid())) {
+                    if (attorDTO.stars.containsKey(mAuth.getCurrentUser().getUid())) {
                         // Unstar the post and remove self from stars
-                        imageDTO.starCount = imageDTO.starCount - 1;
-                        imageDTO.stars.remove(mAuth.getCurrentUser().getUid());
+                        attorDTO.starCount = attorDTO.starCount - 1;
+                        attorDTO.stars.remove(mAuth.getCurrentUser().getUid());
                     } else {
                         // Star the post and add self to stars
-                        imageDTO.starCount = imageDTO.starCount + 1;
-                        imageDTO.stars.put(mAuth.getCurrentUser().getUid(), true);
+                        attorDTO.starCount = attorDTO.starCount + 1;
+                        attorDTO.stars.put(mAuth.getCurrentUser().getUid(), true);
                     }
 
                     // Set value and report transaction success
-                    mutableData.setValue(imageDTO);
+                    mutableData.setValue(attorDTO);
                     return Transaction.success(mutableData);
                 }
 
@@ -147,12 +147,12 @@ public class BoardActivity2 extends AppCompatActivity {
 
         private void delete_content(final int position) {
 
-            storage.getReference().child("images").child(imageDTOs.get(position).imageName).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            storage.getReference().child("attorney").child(attorDTOs.get(position).imageName).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
 
                     //storage의 이미지 파일을 먼저 삭제 성공한 경우, database의 관련 텍스트 데이터를 함께 삭제
-                    database.getReference().child("images").child(uidLists.get(position)).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    database.getReference().child("attorney").child(uidLists.get(position)).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
                             Toast.makeText(BoardActivity2.this, "삭제가 완료되었습니다.", Toast.LENGTH_SHORT).show();
